b4abb0be438baa85dd19f633598d4b68f8f50fa3
Shaktidhar <sdmohapatra7@gmail.com>
2023-06-21 13:13:16 +0530

    Add Shoping cart prject practice starting folder structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import ParentComponent from './components/ParentComponent';
+// import ParentComponent from './components/ParentComponent';
 import './App.css';
+import { useState } from 'react';
+import Items from './components/Items';
+import Navbar from './components/Navbar';
 
 function App() {
+  const [total, setTotal] = useState(0);
+  const [item, setItem] = useState(0);
   return (
     <div className="App">
-      <ParentComponent />
+      {/* <ParentComponent /> */}
+      <h2>Shopping Cart</h2>
+      <Navbar />
+      <Items />
     </div>
   );
 }
